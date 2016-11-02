e476ede3b11f10e47680335e561c2cd59ba1cfb5
Justin Doan <jmdoan1@gmail.com>
2016-11-01 20:33:00 -0400

    Initial Commit

---
+++ Bitcoin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bitcoin
+//
+//  Created by Justin Doan on 11/1/16.
+//  Copyright © 2016 Justin Doan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
