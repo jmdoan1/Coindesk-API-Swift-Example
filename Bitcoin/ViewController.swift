f14e228a6246603bc66d070fa627bb1277590a5a
Justin Doan <jmdoan1@gmail.com>
2016-11-02 15:17:28 -0400

    Changed method for loading data in table view

--- Bitcoin/ViewController.swift
+++ Bitcoin/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     @IBOutlet var tableView: UITableView!
     
     var currencies: [[String:Any]] = []
+    
+    var rates: [(code: String, country: String, rate: Double, ISO: String)] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,10 +40,45 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             
             let json = try! JSONSerialization.jsonObject(with: data, options: .allowFragments) as! [[String:Any]]
             self.currencies = json
-            self.tableView.reloadData()
+            self.getAllRates()
         }
         
         task.resume()
+    }
+    
+    func getAllRates() {
+        rates = []
+        for currency in currencies {
+            let code = currency["currency"] as! String
+            let country = currency["country"] as! String
+            
+            let url = URL(string: "https://api.coindesk.com/v1/bpi/currentprice/\(code).json")
+            
+            let task = URLSession.shared.dataTask(with: url!) { data, response, error in
+                guard error == nil else {
+                    print(error)
+                    return
+                }
+                guard let data = data else {
+                    print("Data is empty")
+                    return
+                }
+                
+                let json = try! JSONSerialization.jsonObject(with: data, options: .allowFragments) as! [String:Any]
+                print(json)
+                let bpi = json["bpi"] as! [String:Any]
+                let codeInfo = bpi[code] as! [String:Any]
+                let rate = codeInfo["rate_float"] as! Double
+                
+                let time = json["time"] as! [String:Any]
+                let ISO = time["updatedISO"] as! String
+                
+                self.rates.append((code: code, country: country, rate: rate, ISO: ISO))
+                self.tableView.reloadData()
+            }
+            task.resume()
+            
+        }
     }
 
     ////does not change before return is called
@@ -82,47 +119,50 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return currencies.count
+        return rates.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         
-        let currency = currencies[indexPath.row]
-        let code = currency["currency"] as! String
-        let country = currency["country"] as! String
-        
-        cell.textLabel?.text = code + ": " + country
-        
-        ////does not change rate before return is called
-        //cell.detailTextLabel?.text = "\(getRate(code: code))"
-        ////putting full function in here for now
-        let url = URL(string: "https://api.coindesk.com/v1/bpi/currentprice/\(code).json")
-        
-        let task = URLSession.shared.dataTask(with: url!) { data, response, error in
-            guard error == nil else {
-                print(error)
-                return
-            }
-            guard let data = data else {
-                print("Data is empty")
-                return
-            }
-            
-            let json = try! JSONSerialization.jsonObject(with: data, options: .allowFragments) as! [String:Any]
-            //print(json)
-            let bpi = json["bpi"] as! [String:Any]
-            let code = bpi[code] as! [String:Any]
-            let rate = code["rate_float"] as! Double
-            //print("RATE:  \(code["rate"]!)")
-            
-            cell.detailTextLabel?.text = "\(rate)"
-            
-        }
-        task.resume()
+//        let currency = currencies[indexPath.row]
+//        let code = currency["currency"] as! String
+//        let country = currency["country"] as! String
+//        
+//        cell.textLabel?.text = code + ": " + country
+//        
+//        ////does not change rate before return is called
+//        //cell.detailTextLabel?.text = "\(getRate(code: code))"
+//        ////putting full function in here for now
+//        let url = URL(string: "https://api.coindesk.com/v1/bpi/currentprice/\(code).json")
+//        
+//        let task = URLSession.shared.dataTask(with: url!) { data, response, error in
+//            guard error == nil else {
+//                print(error)
+//                return
+//            }
+//            guard let data = data else {
+//                print("Data is empty")
+//                return
+//            }
+//            
+//            let json = try! JSONSerialization.jsonObject(with: data, options: .allowFragments) as! [String:Any]
+//            //print(json)
+//            let bpi = json["bpi"] as! [String:Any]
+//            let code = bpi[code] as! [String:Any]
+//            let rate = code["rate_float"] as! Double
+//            //print("RATE:  \(code["rate"]!)")
+//            
+//            cell.detailTextLabel?.text = "\(rate)"
+//            
+//        }
+//        task.resume()
         
         ///////
         
+        let rate = rates[indexPath.row]
+        cell.textLabel?.text = rate.code + " - " + rate.country
+        cell.detailTextLabel?.text = "\(rate.rate) - Updated \(rate.ISO)"
         
         return cell
     }
